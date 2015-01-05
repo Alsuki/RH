e55e659e29fd371f2998d854e136af1b576df7b2
Acer <Acer@Acer-pc>
2015-01-05 21:05:29 +0000

    limpeza de codigo

--- src/java/RH_beans/Auth.java
+++ src/java/RH_beans/Auth.java
@@ -1,7 +1,5 @@
 package RH_beans;
 
-import javax.servlet.RequestDispatcher;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
