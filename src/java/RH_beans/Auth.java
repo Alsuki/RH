c4b2852f9512f054aed96f34597706f3f4ba6aa9
alvega <alvega@193.137.224.139>
2015-01-06 19:42:27 +0000

    Correccao servlet java beans

--- src/java/RH_beans/Auth.java
+++ src/java/RH_beans/Auth.java
@@ -1,16 +1,16 @@
-package RH_beans;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package RH_beans;
+
 /**
  *
- * @author Acer
+ * @author alvega
  */
 public class Auth extends javax.servlet.http.HttpServlet {
-           
+            
    public void setLoginUser (String LogUser) {
        this.LoginUser = LogUser; 
    }
@@ -19,7 +19,7 @@ public class Auth extends javax.servlet.http.HttpServlet {
        this.LoginPass = LogPass;
    }
    
-   public boolean getLogin(){
+   private boolean getLogin(){
        // corrigido o construtor para apenas rentornar verdadiro/falso.
         this.ErrorDetected = "";
         if (CheckLogin(this.LoginUser, this.LoginPass).contains("error")) {
@@ -31,7 +31,7 @@ public class Auth extends javax.servlet.http.HttpServlet {
         }
    }
    
-   public String getError(){
+   private String getError(){
        return this.ErrorDetected;
    }
    
@@ -43,28 +43,6 @@ public class Auth extends javax.servlet.http.HttpServlet {
    private String LoginPass;
    private String ErrorDetected;
    
-   protected void processRequest(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
-           throws javax.servlet.ServletException, java.io.IOException {
-       response.setContentType("text/html/charset=UTF-8");
-       java.io.PrintWriter out = response.getWriter();
-       this.LoginUser = request.getParameter("Name");
-       this.LoginPass = request.getParameter("Password");
-       try {
-           if (this.getLogin()){
-               javax.servlet.http.HttpSession session = request.getSession();
-               session.setAttribute("user", LoginUser);
-               javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("rh");
-               rd.forward(request, response);
-           }else {
-               javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
-               rd.include(request, response);
-           }
-               
-       } finally {
-           out.close();
-       }
-   }
-
    private boolean User(String user, boolean n) {   
     //n tem que ser "true" na primeira chamada
     boolean verify = false;
@@ -155,5 +133,75 @@ public class Auth extends javax.servlet.http.HttpServlet {
           status = "error;User or Password were incorrect. Please try again!";
       }
       return status;
-  } 
+    }
+   
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+           throws javax.servlet.ServletException, java.io.IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        java.io.PrintWriter out = response.getWriter();
+        this.LoginUser = request.getParameter("Name");
+        this.LoginPass = request.getParameter("Password");
+        try {           
+            if (this.getLogin()){
+               javax.servlet.http.HttpSession session = request.getSession();
+               session.setAttribute("user", LoginUser);
+               javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("rh");
+               rd.forward(request, response);
+           }else {
+               javax.servlet.RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
+               rd.include(request, response);
+           }
+               
+       } finally {
+           out.close();
+       }
+    }
+
+ // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, java.io.IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, java.io.IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
 }
